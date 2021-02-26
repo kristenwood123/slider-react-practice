00550e447c015d511196899f92b862159c999860
Kristen <kristenwood123@gmail.com>
2021-02-26 13:53:43 -0800

    slider setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 
 function App() {
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <section className='section'>
+    <div className="title">
+      <h2>
+        <span>/</span>Reviews
+      </h2>
     </div>
+    <div className="section-center">
+      {people.map((person, personIndex) => {
+        const {id, image, name, title, quote} = person;
+        return <article key={id}>
+          <img src={image} alt={name} className='person-img'/>
+          <h4>{name}</h4>
+          <p className='title'>{title}</p>
+          <p className="text">{quote}</p>
+          <FaQuoteRight className='icon'></FaQuoteRight>
+        </article>
+      })}
+      <button className='prev'>
+        <FiChevronLeft/>
+      </button>
+      <button className='next'>
+        <FiChevronRight/>
+      </button>
+    </div>
+  </section>
   );
 }
 
